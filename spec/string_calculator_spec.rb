1d138bbb39e0dfc32a9547d9496b6963cacd3e0a
ng2189-sowmya <80737663+ng2189-sowmya@users.noreply.github.com>
2024-05-27 15:59:02 +0530

    unknow amount of numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -18,9 +18,7 @@ describe 'String Calculator' do
         expect(string_calculator.add).to eq('0')
       end
     end
-  end
 
-  describe '#add' do
     context 'with positive numbers' do
       it 'should return sum' do
         string_calculator = StringCalculator.new('', '1', '1,2')
@@ -28,5 +26,16 @@ describe 'String Calculator' do
         expect(string_calculator.add).to eq('0, 1, 3')
       end
     end
+
+    context 'with unknow amount numbers' do
+      it 'should return sum' do
+        string_calculator = StringCalculator.new('', '1', '1,2', '7,8,9,3')
+
+        expect(string_calculator.add).to eq('0, 1, 3, 27')
+      end
+    end
+
   end
+
+
 end
